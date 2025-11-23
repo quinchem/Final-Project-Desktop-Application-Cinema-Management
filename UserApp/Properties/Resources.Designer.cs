fcecb38070a641b820585219b0eca877229757f0
Quynh Tram Nguyen <nguyenquynhtram1805@gmail.com>
2025-11-23 17:39:57 +0700

    sửa

--- UserApp/Properties/Resources.Designer.cs
+++ UserApp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace UserApp.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -126,6 +126,16 @@ namespace UserApp.Properties {
         public static System.Drawing.Bitmap DatlaiMK {
             get {
                 object obj = ResourceManager.GetObject("DatlaiMK", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap DatVe {
+            get {
+                object obj = ResourceManager.GetObject("DatVe", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
