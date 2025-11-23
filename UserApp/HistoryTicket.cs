34c0bda689b86a253115e21c8f6647e756712a99
lele <linhle.31231023748@st.ueh.edu.vn>
2025-11-23 14:24:03 +0700

    update form danh sách lịch sử đặt vé

---
+++ UserApp/HistoryTicket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserApp
+{
+    public partial class HistoryTicket : UserControl
+    {
+        public HistoryTicket()
+        {
+            InitializeComponent();
+            // Thêm các cột vào dgvHistoryTicket
+            dgvHistoryTicket.Columns.Add("STT", "STT");
+            dgvHistoryTicket.Columns.Add("MaDatVe", "Mã đặt vé");
+            dgvHistoryTicket.Columns.Add("TenPhim", "Tên phim");
+            dgvHistoryTicket.Columns.Add("SuatChieu", "Suất chiếu");
+            dgvHistoryTicket.Columns.Add("NgayDatVe", "Ngày đặt vé");
+            dgvHistoryTicket.Columns.Add("TongTien", "Tổng tiền");
+            dgvHistoryTicket.Columns.Add("TicketCode", "Ticket code");
+
+            // Thêm cột nút Xem chi tiết
+            DataGridViewButtonColumn btnXemChiTiet = new DataGridViewButtonColumn();
+            btnXemChiTiet.HeaderText = "Xem chi tiết";
+            btnXemChiTiet.Text = "XEM";
+            btnXemChiTiet.UseColumnTextForButtonValue = true;
+            dgvHistoryTicket.Columns.Add(btnXemChiTiet);
+        }
+    }
+}
