cce433b11f255f104161f35e7f6e9b94dca1b862
lele <linhle.31231023748@st.ueh.edu.vn>
2025-11-23 21:39:24 +0700

    update các form profile

--- UserApp/HistoryTicket.Designer.cs
+++ UserApp/HistoryTicket.Designer.cs
@@ -31,66 +31,105 @@
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            dgvHistoryTicket = new Guna.UI2.WinForms.Guna2DataGridView();
+            dgvHistoryTicket = new DataGridView();
+            STT = new DataGridViewTextBoxColumn();
+            TenPhim = new DataGridViewTextBoxColumn();
+            SuatChieu = new DataGridViewTextBoxColumn();
+            NgayDatVe = new DataGridViewTextBoxColumn();
+            TongTien = new DataGridViewTextBoxColumn();
+            TicketCode = new DataGridViewTextBoxColumn();
+            XemChiTiet = new DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)dgvHistoryTicket).BeginInit();
             SuspendLayout();
             // 
             // dgvHistoryTicket
             // 
-            dataGridViewCellStyle1.BackColor = Color.White;
-            dgvHistoryTicket.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(100, 88, 255);
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle2.ForeColor = Color.White;
+            dgvHistoryTicket.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvHistoryTicket.BackgroundColor = Color.FromArgb(236, 230, 224);
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(198, 198, 200);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvHistoryTicket.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvHistoryTicket.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvHistoryTicket.Columns.AddRange(new DataGridViewColumn[] { STT, TenPhim, SuatChieu, NgayDatVe, TongTien, TicketCode, XemChiTiet });
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(236, 230, 224);
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 10.2F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
             dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            dgvHistoryTicket.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            dgvHistoryTicket.ColumnHeadersHeight = 4;
-            dgvHistoryTicket.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.White;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            dgvHistoryTicket.DefaultCellStyle = dataGridViewCellStyle3;
-            dgvHistoryTicket.GridColor = Color.FromArgb(231, 229, 255);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvHistoryTicket.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvHistoryTicket.Dock = DockStyle.Fill;
+            dgvHistoryTicket.GridColor = Color.Gray;
             dgvHistoryTicket.Location = new Point(0, 0);
             dgvHistoryTicket.Name = "dgvHistoryTicket";
-            dgvHistoryTicket.RowHeadersVisible = false;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(198, 198, 200);
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 10.2F);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dgvHistoryTicket.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             dgvHistoryTicket.RowHeadersWidth = 62;
             dgvHistoryTicket.Size = new Size(1328, 552);
             dgvHistoryTicket.TabIndex = 0;
-            dgvHistoryTicket.ThemeStyle.AlternatingRowsStyle.BackColor = Color.White;
-            dgvHistoryTicket.ThemeStyle.AlternatingRowsStyle.Font = null;
-            dgvHistoryTicket.ThemeStyle.AlternatingRowsStyle.ForeColor = Color.Empty;
-            dgvHistoryTicket.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.Empty;
-            dgvHistoryTicket.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.Empty;
-            dgvHistoryTicket.ThemeStyle.BackColor = Color.White;
-            dgvHistoryTicket.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(100, 88, 255);
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.Font = new Font("Segoe UI", 9F);
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.ForeColor = Color.White;
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dgvHistoryTicket.ThemeStyle.HeaderStyle.Height = 4;
-            dgvHistoryTicket.ThemeStyle.ReadOnly = false;
-            dgvHistoryTicket.ThemeStyle.RowsStyle.BackColor = Color.White;
-            dgvHistoryTicket.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            dgvHistoryTicket.ThemeStyle.RowsStyle.Font = new Font("Segoe UI", 9F);
-            dgvHistoryTicket.ThemeStyle.RowsStyle.ForeColor = Color.FromArgb(71, 69, 94);
-            dgvHistoryTicket.ThemeStyle.RowsStyle.Height = 33;
-            dgvHistoryTicket.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            dgvHistoryTicket.ThemeStyle.RowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            // 
+            // STT
+            // 
+            STT.HeaderText = "STT";
+            STT.MinimumWidth = 8;
+            STT.Name = "STT";
+            // 
+            // TenPhim
+            // 
+            TenPhim.HeaderText = "Tên Phim";
+            TenPhim.MinimumWidth = 8;
+            TenPhim.Name = "TenPhim";
+            // 
+            // SuatChieu
+            // 
+            SuatChieu.HeaderText = "Suất Chiếu";
+            SuatChieu.MinimumWidth = 8;
+            SuatChieu.Name = "SuatChieu";
+            // 
+            // NgayDatVe
+            // 
+            NgayDatVe.HeaderText = "Ngày Đặt Vé";
+            NgayDatVe.MinimumWidth = 8;
+            NgayDatVe.Name = "NgayDatVe";
+            // 
+            // TongTien
+            // 
+            TongTien.HeaderText = "Tổng Tiền";
+            TongTien.MinimumWidth = 8;
+            TongTien.Name = "TongTien";
+            // 
+            // TicketCode
+            // 
+            TicketCode.HeaderText = "Ticket Code";
+            TicketCode.MinimumWidth = 8;
+            TicketCode.Name = "TicketCode";
+            // 
+            // XemChiTiet
+            // 
+            XemChiTiet.HeaderText = "Xem Chi Tiết";
+            XemChiTiet.MinimumWidth = 8;
+            XemChiTiet.Name = "XemChiTiet";
+            XemChiTiet.Text = "Xem";
+            XemChiTiet.ToolTipText = "Xem";
+            XemChiTiet.UseColumnTextForButtonValue = true;
             // 
             // HistoryTicket
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(236, 230, 224);
+            BackColor = Color.White;
             Controls.Add(dgvHistoryTicket);
             Name = "HistoryTicket";
             Size = new Size(1328, 552);
@@ -100,6 +139,13 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2DataGridView dgvHistoryTicket;
+        private DataGridView dgvHistoryTicket;
+        private DataGridViewTextBoxColumn STT;
+        private DataGridViewTextBoxColumn TenPhim;
+        private DataGridViewTextBoxColumn SuatChieu;
+        private DataGridViewTextBoxColumn NgayDatVe;
+        private DataGridViewTextBoxColumn TongTien;
+        private DataGridViewTextBoxColumn TicketCode;
+        private DataGridViewButtonColumn XemChiTiet;
     }
 }
