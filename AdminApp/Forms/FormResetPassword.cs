aa500bf282d4f994b6229fef300a4af3bb88c43d
Giang Lâm Quỳnh <gianglam.31231023875@gmail.com>
2025-11-27 21:06:01 +0700

    thêm đăng nhập admin

---
+++ AdminApp/Forms/FormResetPassword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminApp.Forms
+{
+    public partial class FormResetPassword : Form
+    {
+        public FormResetPassword()
+        {
+            InitializeComponent();
+        }
+        private AdminMainForm parentForm;
+        public FormResetPassword(AdminMainForm parent)
+        {
+            InitializeComponent();
+            parentForm = parent;
+        }
+
+    }
+}
