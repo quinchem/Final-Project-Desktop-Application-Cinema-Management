eedc50376d69990c17d42b529047c07612a9daad
Lê Đoan Thy <ledoanthy@gmail.com>
2025-11-29 23:56:11 +0700

    chỉnh user + tk1

--- AdminApp/Program.cs
+++ AdminApp/Program.cs
@@ -10,7 +10,7 @@
             // demo – sau này bạn lấy từ form login
             string staff_id = "S01";
 
-            Application.Run(new AdminMainForm(staff_id));
+            Application.Run(new FormStatistics2());
         }
     }
 }
