0ee9c71bf1509fee2a69e88d31bced38962d8200
Lê Đoan Thy <ledoanthy@gmail.com>
2025-11-30 05:06:08 +0700

    them tk3

--- AdminApp/Forms/FormStatistics3.cs
+++ AdminApp/Forms/FormStatistics3.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Guna.Charts.WinForms;
+using SharedData.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,253 @@ namespace AdminApp
 {
     public partial class FormStatistics3 : Form
     {
+
+        private StatisticRepo _statisticsRepo = new StatisticRepo();
+        private bool isFilteringByYear = false;
         public FormStatistics3()
         {
             InitializeComponent();
+            LoadYearCombo();
+            LoadMovieCombo();
+
+            // Thêm event handler cho MouseDown để catch khi user click vào DateTimePicker bị disabled
+            dtpFrom.MouseDown += DateTimePicker_MouseDown;
+            dtpTo.MouseDown += DateTimePicker_MouseDown;
         }
+
+        private void FormStatistics3_Load(object sender, EventArgs e)
+        {
+            dtpFrom.Value = DateTime.Today.AddDays(-30);
+            dtpTo.Value = DateTime.Today;
+            ConfigurePieChartLegend();
+            ReloadAll();
+        }
+
+        private void ConfigurePieChartLegend()
+        {
+            gunaChartPieTopMovies.Legend.Position = LegendPosition.Right;
+            // Ẩn các trục tọa độ cho pie chart
+            gunaChartPieTopMovies.XAxes.Display = false;
+            gunaChartPieTopMovies.YAxes.Display = false;
+        }
+
+        private void LoadYearCombo()
+        {
+            comboYear.Items.Clear();
+            comboYear.Items.Add("Không");
+            comboYear.Items.Add("2023");
+            comboYear.Items.Add("2024");
+            comboYear.Items.Add("2025");
+            comboYear.SelectedIndex = 0;
+        }
+
+        private void LoadMovieCombo()
+        {
+            MovieCombo.Items.Clear();
+            MovieCombo.Items.Add("Tất cả");
+            var movies = _statisticsRepo.GetMoviesCurrentlyShowing();
+            MovieCombo.Items.AddRange(movies.ToArray());
+
+            if (MovieCombo.Items.Count > 0)
+                MovieCombo.SelectedIndex = 0;
+        }
+
+        private void ReloadAll()
+        {
+            LoadRevenueKPI();
+            LoadTopMovieInfo();
+            LoadRevenueLineChart();
+            LoadTopMoviesPieChart();
+            LoadRevenueBarChart();
+        }
+
+        private string GetSelectedMovie()
+        {
+            if (MovieCombo.SelectedItem == null || MovieCombo.SelectedItem.ToString() == "Tất cả")
+                return null;
+
+            return MovieCombo.SelectedItem.ToString();
+        }
+
+        private void LoadRevenueKPI()
+        {
+            string selectedMovie = GetSelectedMovie();
+            var from = dtpFrom.Value;
+            var to = dtpTo.Value;
+
+            var (totalRevenue, totalTickets, avgRevenue) = _statisticsRepo.GetRevenueKPI(from, to, selectedMovie);
+
+            lblTotalRevenue.Text = totalRevenue.ToString("N0") + " VND";
+            lblTotalTickets.Text = totalTickets.ToString();
+            lblAvgRevenue.Text = avgRevenue.ToString("N0") + " VND";
+        }
+
+        private void LoadTopMovieInfo()
+        {
+            string selectedMovie = GetSelectedMovie();
+            var from = dtpFrom.Value;
+            var to = dtpTo.Value;
+
+            var (movieTitle, totalRevenue, totalTickets) = _statisticsRepo.GetTopMovie(from, to, selectedMovie);
+
+            lblTopMovieName.Text = movieTitle;
+        }
+
+        private void LoadRevenueLineChart()
+        {
+            gunaChartRevenue.Datasets.Clear();
+            string selectedMovie = GetSelectedMovie();
+
+            var totalLine = new GunaLineDataset { Label = "Tổng doanh thu", BorderColor = Color.Blue };
+            var topMovieLine = new GunaLineDataset { Label = "Doanh thu phim nổi bật", BorderColor = Color.Red };
+
+            var data = _statisticsRepo.GetRevenueWithTopMovieByDay(dtpFrom.Value, dtpTo.Value, selectedMovie);
+
+            foreach (var item in data)
+            {
+                totalLine.DataPoints.Add(item.date, (double)item.totalRevenue);
+
+                string movieName = item.topMovieName ?? (selectedMovie ?? "N/A");
+                string customLabel = $"{item.date} - {movieName}";
+                topMovieLine.DataPoints.Add(customLabel, (double)item.topMovieRevenue);
+            }
+
+            gunaChartRevenue.Datasets.Add(totalLine);
+            gunaChartRevenue.Datasets.Add(topMovieLine);
+            gunaChartRevenue.Legend.Display = true;
+            gunaChartRevenue.Update();
+        }
+
+        private void LoadTopMoviesPieChart()
+        {
+            gunaChartPieTopMovies.Datasets.Clear();
+            string selectedMovie = GetSelectedMovie();
+
+            var data = _statisticsRepo.GetRevenueForPie(dtpFrom.Value, dtpTo.Value, selectedMovie);
+
+            var top5 = data.Take(5).ToList();
+            var othersTotal = data.Skip(5).Sum(x => x.total);
+
+            var grandTotal = top5.Sum(x => x.total) + othersTotal;
+
+            var pieDataset = new GunaPieDataset();
+
+            foreach (var item in top5)
+            {
+                var percentage = (double)item.total / (double)grandTotal * 100;
+                pieDataset.DataPoints.Add($"{item.movieTitle}: {percentage:F1}%", (double)item.total);
+            }
+
+            if (othersTotal > 0)
+            {
+                var percentage = (double)othersTotal / (double)grandTotal * 100;
+                pieDataset.DataPoints.Add($"Khác: {percentage:F1}%", (double)othersTotal);
+            }
+
+            gunaChartPieTopMovies.Datasets.Add(pieDataset);
+            gunaChartPieTopMovies.Update();
+        }
+
+        private void LoadRevenueBarChart()
+        {
+            gunaChartBar.Datasets.Clear();
+            string selectedMovie = GetSelectedMovie();
+
+            var data = _statisticsRepo.GetRevenueBar(dtpFrom.Value, dtpTo.Value, selectedMovie);
+
+            var bar = new GunaBarDataset { Label = "Doanh thu từng phim" };
+            foreach (var item in data)
+                bar.DataPoints.Add(item.movieTitle, (double)item.total);
+
+            gunaChartBar.Datasets.Add(bar);
+            gunaChartBar.Update();
+        }
+
+        // Event handler khi user click vào DateTimePicker
+        private void DateTimePicker_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isFilteringByYear)
+            {
+                MessageBox.Show("Đang lọc theo năm. Vui lòng chọn 'Không' ở Năm để lọc theo ngày!",
+                                "Thông báo",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+        }
+
+        // VALIDATE DATE: Ngày bắt đầu không được sau ngày kết thúc
+        private bool ValidateDateRange()
+        {
+            if (dtpFrom.Value > dtpTo.Value)
+            {
+                MessageBox.Show("Ngày bắt đầu không được sau ngày kết thúc!",
+                                "Lỗi",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void dtpFrom_ValueChanged(object sender, EventArgs e)
+        {
+            // Chỉ validate và reload nếu đang không lọc theo năm
+            if (!isFilteringByYear)
+            {
+                if (ValidateDateRange())
+                {
+                    ReloadAll();
+                }
+                else
+                {
+                    dtpFrom.Value = dtpTo.Value.AddDays(-1);
+                }
+            }
+        }
+
+        private void dtpTo_ValueChanged(object sender, EventArgs e)
+        {
+            // Chỉ validate và reload nếu đang không lọc theo năm
+            if (!isFilteringByYear)
+            {
+                if (ValidateDateRange())
+                {
+                    ReloadAll();
+                }
+                else
+                {
+                    dtpTo.Value = dtpFrom.Value.AddDays(1);
+                }
+            }
+        }
+
+        private void comboYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedYear = comboYear.SelectedItem?.ToString();
+
+            if (selectedYear == "Không")
+            {
+                // Cho phép chọn ngày
+                isFilteringByYear = false;
+                dtpFrom.Enabled = true;
+                dtpTo.Enabled = true;
+
+                ReloadAll();
+            }
+            else
+            {
+                // Lọc theo năm - DISABLE DateTimePicker
+                isFilteringByYear = true;
+                dtpFrom.Enabled = false;
+                dtpTo.Enabled = false;
+
+                int year = int.Parse(selectedYear);
+                dtpFrom.Value = new DateTime(year, 1, 1);
+                dtpTo.Value = new DateTime(year, 12, 31);
+                ReloadAll();
+            }
+        }
+
+        private void MovieCombo_SelectedIndexChanged(object sender, EventArgs e) => ReloadAll();
     }
 }
---
+++ SharedData/Repositories/StatisticsRepo.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedData.Repositories
+{
+
+        public class StatisticRepo
+        {
+            public (decimal totalRevenue, int totalTickets, decimal avgRevenuePerMovie) GetRevenueKPI(DateTime from, DateTime to, string movieTitle = null)
+            {
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+                using var cmd = conn.CreateCommand();
+
+                string sql = @"
+                SELECT 
+                    SUM(b.total) AS TotalRevenue,
+                    COUNT(b.bill_id) AS TotalTickets,
+                    COUNT(DISTINCT m.movie_id) AS MovieCount
+                FROM bill b
+                JOIN showtime s ON b.showtime_id = s.showtime_id
+                JOIN movie m ON s.movie_id = m.movie_id
+                WHERE substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) BETWEEN @from AND @to
+            ";
+                if (!string.IsNullOrEmpty(movieTitle))
+                    sql += " AND m.title = @movieTitle";
+
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@from", from.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@to", to.ToString("yyyy-MM-dd"));
+                if (!string.IsNullOrEmpty(movieTitle))
+                    cmd.Parameters.AddWithValue("@movieTitle", movieTitle);
+
+                using var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    decimal totalRevenue = reader["TotalRevenue"] != DBNull.Value ? Convert.ToDecimal(reader["TotalRevenue"]) : 0;
+                    int totalTickets = reader["TotalTickets"] != DBNull.Value ? Convert.ToInt32(reader["TotalTickets"]) : 0;
+                    int movieCount = reader["MovieCount"] != DBNull.Value ? Convert.ToInt32(reader["MovieCount"]) : 1;
+                    decimal avgRevenue = movieCount > 0 ? totalRevenue / movieCount : 0;
+                    return (totalRevenue, totalTickets, avgRevenue);
+                }
+                return (0, 0, 0);
+            }
+
+            public List<(string date, decimal totalRevenue, decimal topMovieRevenue, string topMovieName)>
+                GetRevenueWithTopMovieByDay(DateTime fromDate, DateTime toDate, string filterMovie = null)
+            {
+                var result = new List<(string, decimal, decimal, string)>();
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+
+                string sql = @"
+                SELECT 
+                    substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) AS BillDate,
+                    m.title AS MovieTitle,
+                    SUM(b.total) AS Revenue
+                FROM bill b
+                JOIN showtime s ON b.showtime_id = s.showtime_id
+                JOIN movie m ON s.movie_id = m.movie_id
+                WHERE substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) BETWEEN @from AND @to
+            ";
+                if (!string.IsNullOrEmpty(filterMovie))
+                    sql += " AND m.title = @filterMovie";
+
+                sql += " GROUP BY BillDate, m.title ORDER BY BillDate, Revenue DESC";
+
+                using var cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@from", fromDate.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@to", toDate.ToString("yyyy-MM-dd"));
+                if (!string.IsNullOrEmpty(filterMovie))
+                    cmd.Parameters.AddWithValue("@filterMovie", filterMovie);
+
+                var dataByDate = new Dictionary<string, List<(string movie, decimal revenue)>>();
+
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string date = reader.GetString(0);
+                    string movie = reader.GetString(1);
+                    decimal revenue = reader.GetDecimal(2);
+
+                    if (!dataByDate.ContainsKey(date))
+                        dataByDate[date] = new List<(string, decimal)>();
+
+                    dataByDate[date].Add((movie, revenue));
+                }
+
+                foreach (var kvp in dataByDate.OrderBy(x => x.Key))
+                {
+                    string date = kvp.Key;
+                    var movies = kvp.Value;
+                    decimal totalRevenue = movies.Sum(x => x.revenue);
+                    var topMovie = movies.OrderByDescending(x => x.revenue).FirstOrDefault();
+                    result.Add((date, totalRevenue, topMovie.revenue, topMovie.movie ?? "N/A"));
+                }
+
+                return result;
+            }
+
+            public (string movieTitle, decimal totalRevenue, int totalTickets) GetTopMovie(DateTime fromDate, DateTime toDate, string filterMovie = null)
+            {
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+                using var cmd = conn.CreateCommand();
+
+                string sql = @"
+                SELECT m.title, SUM(b.total) AS TotalRevenue, COUNT(b.bill_id) AS TotalTickets
+                FROM bill b
+                JOIN showtime s ON b.showtime_id = s.showtime_id
+                JOIN movie m ON s.movie_id = m.movie_id
+                WHERE substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) BETWEEN @from AND @to
+            ";
+                if (!string.IsNullOrEmpty(filterMovie))
+                    sql += " AND m.title = @filterMovie";
+                sql += " GROUP BY m.movie_id ORDER BY TotalRevenue DESC LIMIT 1";
+
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@from", fromDate.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@to", toDate.ToString("yyyy-MM-dd"));
+                if (!string.IsNullOrEmpty(filterMovie))
+                    cmd.Parameters.AddWithValue("@filterMovie", filterMovie);
+
+                using var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return (reader.GetString(0), reader.GetDecimal(1), reader.GetInt32(2));
+                }
+                return ("N/A", 0, 0);
+            }
+
+            public List<string> GetMoviesCurrentlyShowing()
+            {
+                var list = new List<string>();
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+                using var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT title FROM movie WHERE status='Đang chiếu'";
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                    list.Add(reader.GetString(0));
+                return list;
+            }
+
+            public List<(string movieTitle, decimal total)> GetRevenueForPie(DateTime from, DateTime to, string selectedMovie = null)
+            {
+                var list = new List<(string, decimal)>();
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+                using var cmd = conn.CreateCommand();
+
+                string sql = @"
+                SELECT m.title, SUM(b.total) AS Total
+                FROM bill b
+                JOIN showtime s ON b.showtime_id = s.showtime_id
+                JOIN movie m ON s.movie_id = m.movie_id
+                WHERE substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) BETWEEN @from AND @to
+            ";
+                if (!string.IsNullOrEmpty(selectedMovie))
+                    sql += " AND m.title = @selectedMovie";
+                sql += " GROUP BY m.movie_id ORDER BY Total DESC";
+
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@from", from.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@to", to.ToString("yyyy-MM-dd"));
+                if (!string.IsNullOrEmpty(selectedMovie))
+                    cmd.Parameters.AddWithValue("@selectedMovie", selectedMovie);
+
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                    list.Add((reader.GetString(0), reader.GetDecimal(1)));
+
+                return list;
+            }
+
+            public List<(string movieTitle, decimal total)> GetRevenueBar(DateTime from, DateTime to, string movieTitle = null)
+            {
+                var list = new List<(string, decimal)>();
+                using var conn = DatabaseHelper.GetConnection();
+                conn.Open();
+                using var cmd = conn.CreateCommand();
+
+                string sql = @"
+                SELECT m.title, SUM(b.total) AS Total
+                FROM bill b
+                JOIN showtime s ON b.showtime_id = s.showtime_id
+                JOIN movie m ON s.movie_id = m.movie_id
+                WHERE substr(b.bill_date,7,4)||'-'||substr(b.bill_date,4,2)||'-'||substr(b.bill_date,1,2) BETWEEN @from AND @to
+            ";
+                if (!string.IsNullOrEmpty(movieTitle))
+                    sql += " AND m.title = @movieTitle";
+                sql += " GROUP BY m.movie_id ORDER BY Total DESC";
+
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@from", from.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@to", to.ToString("yyyy-MM-dd"));
+                if (!string.IsNullOrEmpty(movieTitle))
+                    cmd.Parameters.AddWithValue("@movieTitle", movieTitle);
+
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                    list.Add((reader.GetString(0), reader.GetDecimal(1)));
+
+                return list;
+            }
+        }
+    }
