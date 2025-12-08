5037ab9ec800cd96f9954e3c1d2d53abaf526ce4
Quynh Tram Nguyen <nguyenquynhtram1805@gmail.com>
2025-12-08 13:09:56 +0700

    ha

---
+++ SharedData/Repositories/SearchRepo.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using SharedData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SharedData.Repositories
+{
+    public class SearchRepo
+    {
+        private static string ConnStr => DatabaseHelper.GetConnectionString();
+
+        // Hàm tìm phim theo từ khóa (theo title hoặc mô tả)
+        public List<Film> SearchFilms(string keyword)
+        {
+            List<Film> list = new List<Film>();
+
+            if (string.IsNullOrEmpty(keyword))
+                return list;
+
+            string likeKey = "%" + keyword + "%";
+
+            using var conn = new SqliteConnection(ConnStr);
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"
+                SELECT movie_id, title, duration, age_restriction, release_date
+                FROM movie
+                WHERE title LIKE $k OR description LIKE $k
+            ";
+
+            cmd.Parameters.AddWithValue("$k", likeKey);
+
+            using var rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                list.Add(new Film
+                {
+                    movie_id = rd.GetString(0),
+                    title = rd.GetString(1),
+                    duration = rd.GetInt32(2),
+                    age_restriction = rd.GetString(3),
+                    release_date = rd.GetString(4)
+                });
+            }
+
+            return list;
+        }
+    }
+}
---
+++ UserApp/Forms/FormSearch.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UserApp.Forms
+{
+    partial class FormSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            btnTimKiem = new Guna.UI2.WinForms.Guna2Button();
+            txtTimKiem = new Guna.UI2.WinForms.Guna2TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.BackColor = Color.Transparent;
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.Image = Properties.Resources.search;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(1220, 50);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(35, 35);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox1.TabIndex = 12;
+            guna2PictureBox1.TabStop = false;
+            guna2PictureBox1.UseTransparentBackground = true;
+            // 
+            // btnTimKiem
+            // 
+            btnTimKiem.BorderRadius = 6;
+            btnTimKiem.CustomizableEdges = customizableEdges3;
+            btnTimKiem.DisabledState.BorderColor = Color.DarkGray;
+            btnTimKiem.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnTimKiem.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnTimKiem.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnTimKiem.FillColor = Color.FromArgb(245, 131, 35);
+            btnTimKiem.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTimKiem.ForeColor = Color.White;
+            btnTimKiem.Location = new Point(1281, 44);
+            btnTimKiem.Name = "btnTimKiem";
+            btnTimKiem.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnTimKiem.Size = new Size(159, 46);
+            btnTimKiem.TabIndex = 11;
+            btnTimKiem.Text = "Tìm kiếm";
+            // 
+            // txtTimKiem
+            // 
+            txtTimKiem.BorderRadius = 6;
+            txtTimKiem.CustomizableEdges = customizableEdges5;
+            txtTimKiem.DefaultText = "";
+            txtTimKiem.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtTimKiem.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtTimKiem.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtTimKiem.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtTimKiem.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTimKiem.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtTimKiem.ForeColor = Color.FromArgb(64, 64, 64);
+            txtTimKiem.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTimKiem.Location = new Point(434, 44);
+            txtTimKiem.Margin = new Padding(4, 5, 4, 5);
+            txtTimKiem.Name = "txtTimKiem";
+            txtTimKiem.PlaceholderText = "";
+            txtTimKiem.SelectedText = "";
+            txtTimKiem.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtTimKiem.Size = new Size(835, 46);
+            txtTimKiem.TabIndex = 10;
+            txtTimKiem.KeyDown += txtTimKiem_KeyDown;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = AnchorStyles.Top;
+            flowLayoutPanel1.Location = new Point(1, 129);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1885, 679);
+            flowLayoutPanel1.TabIndex = 13;
+            // 
+            // FormSearch
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(92, 124, 150);
+            ClientSize = new Size(1884, 808);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(guna2PictureBox1);
+            Controls.Add(btnTimKiem);
+            Controls.Add(txtTimKiem);
+            Name = "FormSearch";
+            Text = "FormSearch";
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnTimKiem;
+        private Guna.UI2.WinForms.Guna2TextBox txtTimKiem;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
